5b5c3c871707d2861181ad7e4c803b9f02813992
hackerton <wizardlordlam@gmail.com>
2020-09-15 06:42:49 +0800

    added mqtt publish
    Signed-off-by: hackerton <wizardlordlam@gmail.com>

--- App.js
+++ App.js
@@ -9,17 +9,16 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import React, {useEffect, useState} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import {Text, View, Button} from 'react-native';
 import * as Mqtt from 'react-native-native-mqtt';
+import Realm from 'realm';
+import {Buffer} from 'buffer';
 
 const Stack = createStackNavigator();
+
+function encode_mqtt(value) {
+  return Buffer.from(value);
+}
 
 const Home = ({route, navigation}) => {
   const [records, setRecords] = useState([]);
@@ -27,48 +26,73 @@ const Home = ({route, navigation}) => {
     new Mqtt.Client('tcp://broker.hivemq.com:1883'),
   );
 
+  console.log('Refresh');
+
+  // useEffect(() => {
+  //   Realm.open({
+  //     schema: [{name: 'Msgs', properties: {timecode: 'string', msg: 'string'}}],
+  //   }).then((realm) => {
+  //     realm.write(() => {
+  //       realm.create('Msgs', {timecode: 'hllo', msg: 'msgs'});
+  //     });
+  //     console.log(realm.objects('Msgs').length);
+  //   });
+  // });
+
+  // Initialize Mqtt Client
   useEffect(() => {
+    try {
+      client.connect(
+        {
+          clientId: 'PHONE',
+          username: 'hackerton',
+          password: 'hackerton',
+        },
+        (err) => {},
+      );
+    } catch (error) {}
+
     client.on(Mqtt.Event.Error, (error) => {
       console.error('Error Message:', error);
+      alert(`Error message ${error}`);
     });
 
     client.on(Mqtt.Event.Connect, () => {
       console.log('Mqtt Connect');
       client.subscribe(['esptest/1'], [0]);
     });
-
-    client.connect(
-      {
-        clientId: 'PHONE',
-        username: 'hackerton',
-        password: 'hackerton',
-      },
-      (err) => {
-        console.log(err);
-      },
-    );
-
-    return () => client.disconnect();
   }, []);
 
+  // Update array if topic is found
   useEffect(() => {
     client.on(Mqtt.Event.Message, (topic, message) => {
-      console.log('Mqtt Message:', topic, message.toString());
-      setRecords([...records, {timecode: Date.now(), msg: message.toString()}]);
+      console.log(message.toString());
+      if (topic === 'esptest/1') {
+        setRecords([
+          ...records,
+          {timecode: Date.now(), msg: message.toString()},
+        ]);
+      } else if (topic === 'esptest/2') {
+        console.log(message.toString());
+      }
     });
   }, [records]);
 
   return (
     <View>
       {records.map(({timecode, msg}) => {
-        console.log(timecode);
-        return (
-          <View>
-            <Text>Timecode: {timecode}</Text>
-            <Text>Msg: {msg}</Text>
-          </View>
-        );
+        return <Text>{msg}</Text>;
       })}
+      <Button
+        title={'STRING'}
+        onPress={() => {
+          client.publish(
+            'esptest/2',
+            encode_mqtt(JSON.stringify({name: 'alan'})),
+          );
+        }}>
+        Button
+      </Button>
     </View>
   );
 };
@@ -82,7 +106,7 @@ function Register({navigation}) {
 }
 
 function App() {
-  const [route, setRoute] = useState('home');
+  const [route, setRoute] = useState('Home');
 
   return (
     <NavigationContainer>
